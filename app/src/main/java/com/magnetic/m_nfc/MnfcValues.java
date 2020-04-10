cb9b391b944b9fa5b5413c29e1a005f4c1ca5e1c
andrejica <andrejic.aleks91@gmail.com>
2020-04-10 20:27:41 +0200

    Initial commit

---
+++ app/src/main/java/com/magnetic/m_nfc/MnfcValues.java
@@ -0,0 +1,13 @@
+package com.magnetic.m_nfc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MnfcValues extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mnfc_values);
+    }
+}
