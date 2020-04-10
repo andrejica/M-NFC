d7c66cde08aaf12662f173d654455a1f62f87045
andrejica <andrejic.aleks91@gmail.com>
2020-04-10 20:37:26 +0200

    Added activity for M-NFC and switching between Home and M-NFC activities

--- app/src/main/java/com/magnetic/m_nfc/MainActivity.java
+++ app/src/main/java/com/magnetic/m_nfc/MainActivity.java
@@ -1,5 +1,8 @@
 package com.magnetic.m_nfc;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeActivity();
+    }
+
+    private  void changeActivity(){
+        final Button switchToMnfc = findViewById(R.id.startMnfc);
+        switchToMnfc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MnfcValues.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/magnetic/m_nfc/MnfcValues.java
+++ app/src/main/java/com/magnetic/m_nfc/MnfcValues.java
@@ -1,5 +1,8 @@
 package com.magnetic.m_nfc;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,17 @@ public class MnfcValues extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mnfc_values);
+
+        changeToHome();
+    }
+
+    private  void changeToHome(){
+        final Button switchToHome = findViewById(R.id.back_home_mnfc);
+        switchToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
